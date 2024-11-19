5368ab3789f4644c9afc53b1bb99ed72eb37aa4a
sylphhhhhh <153562312+sylphhhhhh@users.noreply.github.com>
2024-11-19 14:08:55 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count > 0 ? count - 1 : 0);
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value || 'Anonymous');
+  };
+
+  const handlePassValue = () => {
+    alert(`Your name is: ${name} and your count is: ${count}`);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', alignItems:'center', padding: '20px' }}>
+      <h1>Halo nama ku, Anonymous</h1>
+      <p>Umur ku, {count} tahun</p>
+      <p>{count}</p>
+
+      <button onClick={handleIncrement} style={buttonStyle}>INCREMENT</button>
+      <button onClick={handleDecrement} style={buttonStyle}>DECREMENT</button>
+      <button onClick={handlePassValue} style={buttonStyle}>PASS VALUE</button>
+
+      <br />
+    </div>
+  );
+}
+
+const buttonStyle = {
+  margin: '5px',
+  padding: '10px 20px',
+  fontSize: '16px',
+  cursor: 'pointer',
+  backgroundColor: '#007bff',
+  color: '#fff',
+  border: 'none',
+  borderRadius: '5px',
+
+};
+
+const inputStyle = {
+  marginTop: '10px',
+  padding: '10px',
+  fontSize: '16px',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+};
+
+export default App;
---
+++ index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the Home Screen!</Text>
+      <Button
+        title="Go to App"
+        onPress={() => router.push('/app')} 
+      />
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
